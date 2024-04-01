99ada1c1da4c5cadf875cd1a8bdfb1b244b5f8b9
buddurid <bbahae624@gmail.com>
2024-04-02 00:07:43 +0100

    minor homepage fixes

--- Checkout.php
+++ Checkout.php
@@ -157,7 +157,6 @@
 <div class="container">
   <main>
     <div class="py-5 text-center">
-      <img class="d-block mx-auto mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
       <h2>Checkout form</h2>
       <p class="lead">Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
     </div>
