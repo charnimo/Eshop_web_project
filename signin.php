970058342a07c82c69a805612cbbea26aa682f57
ilyes <ilyesbenjemaa17@gmail.com>
2024-04-06 12:50:39 +0100

    add class user+ improve security in sign in and up

--- signin.php
+++ signin.php
@@ -1,39 +1,27 @@
 <?php
 session_start();
+
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["signin"])) {
     include 'connection.php';
+    include 'User.php';
 
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-    $result = mysqli_query($conn, $sql);
+    $user = new User($conn);
+    $userData = $user->getUserByUsername($username);
 
-    if (mysqli_num_rows($result) > 0) {
-        $row = mysqli_fetch_assoc($result);
-        //la hash la wedhni
-        if ($row['id'] == 0) {
-            //clear all old sessions:
-            session_destroy();
-            unset($_SESSION['admin']);
-            unset($_SESSION['user']);
-            session_start();
+    if ($userData && password_verify($password, $userData['password'])) {
+        if ($userData['id'] == 0) {
             $_SESSION['admin'] = 1;
-            header("Location: homepage.php");
         } else {
-            //clear all old sessions:
-            session_destroy();
-            unset($_SESSION['admin']);
-            unset($_SESSION['user']);
-            session_start();
-            $_SESSION['user'] = $row['name'];
-            header("Location: homepage.php");
+            $_SESSION['user'] = $userData['name'];
         }
+        header("Location: homepage.php");
     } else {
-        //kif kif add error message
-        echo "User not found!";
+        echo "Incorrect username or password";
     }
 
-    mysqli_close($conn);
+    $conn->close();
 }
 ?>
--- signup.php
+++ signup.php
@@ -1,31 +1,28 @@
 <?php
-
-
+session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["signup"])) {
     include 'connection.php';
+    include 'User.php';
 
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    //check if user exists
-    $sql = "SELECT * FROM users WHERE username = '$username' OR email = '$email'";
-    $result = mysqli_query($conn, $sql);
-    if (mysqli_num_rows($result) > 0) {
+    $user = new User($conn);
+
+    $existingUser = $user->getUserByUsername($username);
+    if ($existingUser) {
         echo "User already exists";
-        exit;
-    }
-    else{
-    $sql = "INSERT INTO user (username, email, password) VALUES ('$username', '$email', '$password')";
-
-    if (mysqli_query($conn, $sql)) {
-        header("Location: login.php");
     } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        $success = $user->createUser($username, $email, $password);
+        if ($success) {
+            header("Location: login.php");
+        } else {
+            echo "Error creating user";
+        }
     }
-}
 
-    mysqli_close($conn);
+    $conn->close();
 }
 ?>
